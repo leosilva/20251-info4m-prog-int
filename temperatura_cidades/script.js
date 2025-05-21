3ac21a94862c6438a7c6daf65d2368b50e1ae51b
= <=>
2025-05-21 13:01:29 -0300

    Exemplo consulta API de temperatura

---
+++ temperatura_cidades/script.js
@@ -0,0 +1,102 @@
+const WWO_CODE = {
+    "113": "Sunny",
+    "116": "PartlyCloudy",
+    "119": "Cloudy",
+    "122": "VeryCloudy",
+    "143": "Fog",
+    "176": "LightShowers",
+    "179": "LightSleetShowers",
+    "182": "LightSleet",
+    "185": "LightSleet",
+    "200": "ThunderyShowers",
+    "227": "LightSnow",
+    "230": "HeavySnow",
+    "248": "Fog",
+    "260": "Fog",
+    "263": "LightShowers",
+    "266": "LightRain",
+    "281": "LightSleet",
+    "284": "LightSleet",
+    "293": "LightRain",
+    "296": "LightRain",
+    "299": "HeavyShowers",
+    "302": "HeavyRain",
+    "305": "HeavyShowers",
+    "308": "HeavyRain",
+    "311": "LightSleet",
+    "314": "LightSleet",
+    "317": "LightSleet",
+    "320": "LightSnow",
+    "323": "LightSnowShowers",
+    "326": "LightSnowShowers",
+    "329": "HeavySnow",
+    "332": "HeavySnow",
+    "335": "HeavySnowShowers",
+    "338": "HeavySnow",
+    "350": "LightSleet",
+    "353": "LightShowers",
+    "356": "HeavyShowers",
+    "359": "HeavyRain",
+    "362": "LightSleetShowers",
+    "365": "LightSleetShowers",
+    "368": "LightSnowShowers",
+    "371": "HeavySnowShowers",
+    "374": "LightSleetShowers",
+    "377": "LightSleet",
+    "386": "ThunderyShowers",
+    "389": "ThunderyHeavyRain",
+    "392": "ThunderySnowShowers",
+    "395": "HeavySnowShowers",
+}
+
+const WEATHER_SYMBOL = {
+    "Unknown": "✨",
+    "Cloudy": "☁️",
+    "Fog": "🌫",
+    "HeavyRain": "🌧",
+    "HeavyShowers": "🌧",
+    "HeavySnow": "❄️",
+    "HeavySnowShowers": "❄️",
+    "LightRain": "🌦",
+    "LightShowers": "🌦",
+    "LightSleet": "🌧",
+    "LightSleetShowers": "🌧",
+    "LightSnow": "🌨",
+    "LightSnowShowers": "🌨",
+    "PartlyCloudy": "⛅️",
+    "Sunny": "☀️",
+    "ThunderyHeavyRain": "🌩",
+    "ThunderyShowers": "⛈",
+    "ThunderySnowShowers": "⛈",
+    "VeryCloudy": "☁️",
+}
+
+$(document).ready(function () {
+  $('#button-submit').on('click', function () {
+    let city = $('#cidade').val().trim();
+
+    if (!city) {
+      alert('Por favor, digite uma cidade.');
+      return;
+    }
+
+    let url = 'https://wttr.in/' + encodeURIComponent(city) + '?format=j1';
+
+    $.getJSON(url, function (data) {
+      let weather = data.current_condition[0];
+      let temp = weather.temp_C;
+      let desc = weather.lang_pt[0].value;
+      let weatherCode = weather.weatherCode
+      let wwo_code_translation = WWO_CODE[weatherCode]
+      let iconSymbol = WEATHER_SYMBOL[wwo_code_translation]
+
+      $('#weather-result').html(`
+        <p><i>${iconSymbol}</i><strong>${city}</strong></p>
+        <p>Temperatura: ${temp}°C</p>
+        <p>Condição: ${desc}</p>
+      `);
+    }).fail(function () {
+      $('#weather-result').html('<p>Erro ao consultar a API do tempo. Verifique o nome da cidade.</p>');
+    });
+  });
+});
