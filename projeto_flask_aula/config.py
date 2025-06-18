16b403bcb222a0fe8951dbfac3b408a9c01f1d9d
= <=>
2025-06-18 11:31:20 -0300

    atualizacao de projeto em flask. Inclusao de banco de dados

--- projeto_flask_aula/config.py
+++ projeto_flask_aula/config.py
@@ -3,4 +3,4 @@ import urllib.parse as parse
 
 class Config:
     SECRET_KEY = secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:{}@127.0.0.1:3306/projeto_flask_aula".format(parse.quote(""))
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:{}@127.0.0.1:3306/projeto_flask_aula".format(parse.quote("")) 
